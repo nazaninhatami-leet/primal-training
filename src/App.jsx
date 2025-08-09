d378a51578906fb21a133d4c3281e6e249c72e24
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-08-09 15:21:03 +0330

    make header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/header';
+import Title from './components/title';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Title />
+    </>
+  );
+}
+
+export default App;
