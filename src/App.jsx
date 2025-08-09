0984ed94ad823fa56ea18b738666dfdd53eddb86
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-08-09 16:22:11 +0330

    make committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from './components/header';
 import Title from './components/title';
+import Committed from './components/committed';
 
 function App() {
   return (
     <>
       <Header />
       <Title />
+      <Committed />
+      <div className='h-1000 bg-indigo-500'></div>
     </>
   );
 }
