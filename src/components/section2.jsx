3d4b1e6fc5066d1b4ed7a8f6a04ed3c5c3ba72df
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-08-10 12:09:23 +0330

    make section 2

--- src/components/section2.jsx
+++ src/components/section2.jsx
@@ -1,17 +1,13 @@
-// import img1 from '../assets/'
+import { Guided } from './guided';
+import Dynamic from './dynamic';
 import Img1 from './img1';
 
 const Section2 = () => {
   return (
     <div className="bg-primary h-126 border-b border-black grid grid-cols-3">
-      <div className="bg-amber-600">1</div>
-      <div className="bg-amber-700 border-l border-black">2</div>
-        <Img1 />
-        {/* <img
-          className="object-cover h-full w-full"
-          src="https://appeal-rice-stuff.figma.site/_assets/v10/4b3ae65ece4bb9cc6a2f518907d80df4631625fe.png"
-          alt=""
-        /> */}
+      <Guided />
+      <Dynamic />
+      <Img1 />
     </div>
   );
 };
--- src/components/title.jsx
+++ src/components/title.jsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <div className="h-42 border-b border-black text-[6.4rem] px-5 font-extrabold tracking-[-0.4rem]">
+    <div className="h-42 border-b border-black text-[6.5rem] px-5 font-extrabold tracking-[-0.4rem]">
       <span className="">TRAIN HARD. </span>
       <span className="text-secondary"> LIVE BETTER</span>
     </div>
