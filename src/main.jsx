2612d40d68a2c1ccfdaeeb8f83698c11a3095d5c
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-08-10 14:28:58 +0330

    display discover image

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<App />} />
+        <Route path='/home?/' element={<App />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
