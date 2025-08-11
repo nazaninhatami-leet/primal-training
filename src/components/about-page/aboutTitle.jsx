6462a703fcb2a547233e0720cc5f5c0eedd29274
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-08-11 09:54:38 +0330

    create about title

---
+++ src/components/about-page/aboutTitle.jsx
@@ -0,0 +1,8 @@
+const AboutTitle = () => {
+  return (
+    <div className="h-[10.55rem] border-b font-extrabold border-black text-[7.2rem] tracking-[-0.40rem] pl-5">
+      ABOUT US
+    </div>
+  );
+};
+export default AboutTitle;
---
+++ src/components/about-page/index.jsx
@@ -0,0 +1,12 @@
+import Header from '../home-page/header';
+import AboutTitle from './aboutTitle';
+
+const AboutPage = () => {
+  return (
+    <>
+      <Header />
+      <AboutTitle />
+    </>
+  );
+};
+export default AboutPage;
