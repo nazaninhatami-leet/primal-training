29128b8c39c6f43fb58e822b8d0217e8d78ba828
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-08-11 15:08:18 +0330

    create book.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router';
-// import Header from './components/homepage/header.jsx';
 import AboutPage from './components/about-page';
+import ReservePage from './components/reserve-page/index.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route index element={<App />} />
         <Route path="about/" element={<AboutPage />} />
-        {/* <Route path="reserve/" element={<Header />} /> */}
+        <Route path="reserve/" element={<ReservePage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
