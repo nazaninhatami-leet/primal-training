78b46b72c0c702763cd6bf33d90fb022b00c9f0b
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-08-11 14:52:48 +0330

    add footer to about page

--- src/components/about-page/index.jsx
+++ src/components/about-page/index.jsx
@@ -4,6 +4,7 @@ import AboutTitle from './aboutTitle';
 import Gym from './gym';
 import Reconnect from './reconnect';
 import Join from './join';
+import Footer from '../home-page/footer';
 
 const AboutPage = () => {
   return (
@@ -14,7 +15,8 @@ const AboutPage = () => {
       <Gym />
       <Reconnect />
       <Join />
-      <div className="bg-amber-200 h-[48rem]"></div>
+      <Footer />
+      {/* <div className="bg-amber-200 h-[48rem]"></div> */}
     </>
   );
 };
