0ac34cf83187699021c65a52dc0c215494140bf2
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-08-11 15:09:38 +0330

    create book.jsx

---
+++ src/components/reserve-page/book.jsx
@@ -0,0 +1,12 @@
+const Book = () => {
+  return (
+    <div
+      className="h-42 border-b border-black text-[7.2rem] px-5 font-extrabold tracking-[-0.4rem]"
+      style={{ fontFamily: "'Anek Tamil', sans-serif" }}
+    >
+      <span className="">BOOK A</span>
+      <span className="text-secondary">&nbsp;SESSION</span>
+    </div>
+  );
+};
+export default Book;
---
+++ src/components/reserve-page/index.jsx
@@ -0,0 +1,12 @@
+import Header from '../home-page/header';
+import Book from './book';
+
+const ReservePage = () => {
+  return (
+    <>
+      <Header />
+      <Book />
+    </>
+  );
+};
+export default ReservePage;
